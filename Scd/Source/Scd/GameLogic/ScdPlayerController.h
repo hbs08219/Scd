390fbd2ecc4acfe15564310c01f3464a91926480
hbs08219 <534108854@qq.com>
2023-01-18 22:20:34 +0800

    基础设施

---
+++ Scd/Source/Scd/GameLogic/ScdPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ScdPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCD_API AScdPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
